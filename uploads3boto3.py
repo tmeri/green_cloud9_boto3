f6843951e5ed578ea206486aed3c04b2e1aafab9
tmeri <77613943+tmeri@users.noreply.github.com>
2022-11-10 22:35:03 -0500

    Update uploads3boto3.py

--- uploads3boto3.py
+++ uploads3boto3.py
@@ -1,7 +1,3 @@
-#import boto3
-#s3 = boto3.resource('s3')
-#s3.meta.client.upload_file (r'C:\Users\temes\Desktop\s3_upload_boto3.txt','boto3pythonaws','hello.txt')
-
 import boto3
 
 s3 = boto3.client ('s3')
