0b1e62048e23fbaf5bdad84bcad2e823b4f26d86
Temesgen Meri <77613943+tmeri@users.noreply.github.com>
2022-11-20 23:00:05 -0500

    Update lambda_sqs.py

--- lambda_sqs.py
+++ lambda_sqs.py
@@ -1,19 +1,19 @@
-#import json
+import json
 import boto3
-#from datetime import datetime
+from datetime import datetime
 client = boto3.client('sqs')
 
 def lambda_handler(event, context):
         #Get the current date & time
-    #response = "The time and date is: "
-  #  date_time = datetime.now()
+    response = "The time and date is: "
+    date_time = datetime.now()
     
     #send sqs message with the current date & time
     message = client.send_message(
         QueueUrl='https://sqs.us-east-1.amazonaws.com/774347611285/Green-Team-Queue-Week15',
-  #      MessageBody=("This was sent on: " + str(date_time.strftime('%Y-%m-%d %H:%M:%S')))
+    MessageBody=("This was sent on: " + str(date_time.strftime('%Y-%m-%d %H:%M:%S')))
         )
     return {
         'statusCode': 200,
-  #      'body': json.dumps(message, indent=2)
+        'body': json.dumps(message, indent=2)
     }
